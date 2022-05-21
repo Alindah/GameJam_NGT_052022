aff6ddacdd81d23514b912103284d827bd075ced
Alinda <Alindah@users.noreply.github.com>
2022-05-21 13:00:13 -0400

    Spawn items upon pressing space

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject[] item;
+    
+    private int maxNumOfItemTypes;
+
+    private void Start()
+    {
+        maxNumOfItemTypes = item.Length;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnItem();
+        }
+    }
+
+    // Instantiate the itemPool
+    private void SpawnItem()
+    {
+        int randomIndex = Random.Range(0, maxNumOfItemTypes);
+        Instantiate(item[randomIndex]);
+        Debug.Log("spawned item " + randomIndex);
+    }
+}
