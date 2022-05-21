09ff3ea70c7cb8780eb6bb7ab1b2238853b340c2
Allan Millar <33874328+travelingnight@users.noreply.github.com>
2022-05-21 14:39:24 -0500

    Added flip functionality
    Altered to flip player character horizontally when changing directions.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 20f;
     public float maxDistanceFromCenter = 10.0f;
-
     public float horizontalInput { get; private set; }
+    private bool facingRight = false;
 
     // Update is called once per frame
     void Update()
     {
         //Get player input
         horizontalInput = Input.GetAxis("Horizontal");
-        
+        if (horizontalInput<0 && facingRight)
+        {
+            flip();
+            facingRight = false;
+        }
+        if (horizontalInput > 0 && !facingRight) {
+            flip();
+            facingRight = true;
+        }
+
         if (Input.GetButton("Horizontal")) {
             if (Mathf.Abs(transform.position.x) < maxDistanceFromCenter) {
                 transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * movementSpeed);
@@ -21,5 +30,10 @@ public class PlayerController : MonoBehaviour
                 transform.Translate(Vector2.left * horizontalInput * Time.deltaTime * movementSpeed);
             }
         }
+    }
+
+    private void flip()
+    {
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
     }
 }
